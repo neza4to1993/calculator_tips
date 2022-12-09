098dcf2885383c1581117fc6937859b526fea2d1
Maksim <mack.pustovit@yandex.ru>
2022-12-09 12:06:37 +0300

    logic

--- src/modules/script.js
+++ src/modules/script.js
@@ -3,27 +3,18 @@ const totalSum = document.querySelector('.total');
 const checkSum = document.querySelector(".payment__input");
 		checkSum.addEventListener("input", addTotal);
 const people = document.querySelector(".people__input");
-		people.addEventListener("input", addingPeopleTotal);
+		people.addEventListener("focusin", checkPercent);
 const percentage = document.getElementById('Custom');
 		percentage.addEventListener('input', createsPercentage);
 const allTips = document.querySelectorAll('.tips__item');
 		allTips.forEach(element => {element.addEventListener('click', addToAmount);});
 const btnReset = document.getElementById('btnReset');
 		btnReset.addEventListener('click', removeAll);
+		
 
 
-function addToAmount(result) {
-	const total = totalSum.innerText / 100 * this.value;
-	const amount = Number(total / people.value);
-	const Bill = Number(checkSum.value);
-	if (result){
-		console.log('sya tut');
-	}else{
-		resultAmount.innerText = amount.toFixed(2);
-		console.log(this.value);
-		console.log(totalSum.innerText);
-		totalSum.innerText = (amount + Bill).toFixed(2);
-	}
+function addToAmount() {
+	addingTotal(this);
 }
 
 function addTotal() {
@@ -40,28 +31,46 @@ if (sum === ""){
 
 function createsPercentage() {
 	maxLengthCheck(this);
-		const percent = this.value;
-		const total = totalSum.innerText / 100 * this.value;
-		const result = Number(total / people.value);
-		console.log(result);
-if(percent > 100){
-	this.value = '100';
-};
-	addToAmount(result);
-		// 	resultAmount.innerText = result.toFixed(2);
+		if(this.value > 100){
+			this.value = '100';
+		};
+		addingTotal(this);
 }
 
-function addingPeopleTotal() {
-	maxLengthCheck(this);
-		people.addEventListener("focusout", () =>  {
-			if (people.value === ""){
-				people.value = "1";
-			}
-		});
-	if(checkSum.value !== "" & people.value !== ""){
-		const result = (checkSum.value / people.value).toFixed(2);
-		totalSum.innerText = result;
+function checkPercent() {
+	const peopple = Number(this.value);
+	const checkSumm = Number(checkSum.value);
+	const SumAmount = Number(resultAmount.innerText);
+	const total = Number(totalSum.innerText);
+	const percent = ((total * peopple) - checkSumm) / (checkSumm / 100);
+		people.addEventListener('input', addingPeopleTotal);
+			return percent, total, SumAmount, checkSumm;
+
+	function addingPeopleTotal() {
+		maxLengthCheck(people);
+			people.addEventListener("focusout", () =>  {
+				if (people.value === "" || people.value === "0"){
+					alert("Specify the number of people !");
+					people.value = "1";
+				}
+			});
+		if(checkSum.value !== "" & people.value !== ""){
+		const peopple = Number(this.value);
+			resultAmount.innerText = (Math.ceil(percent) / peopple).toFixed(2); // push Tip Amount 
+			totalSum.innerText = ((checkSumm + percent)  / peopple).toFixed(2); // push Total 
+
+		}
 	}
+	}
+
+function addingTotal(val) {
+	const checkSumm = Number(checkSum.value);
+	const peoplee = Number(people.value);
+	const total = checkSum.value / 100 * val.value;
+	const summ = (checkSumm + total) / peoplee;
+	const amount = total / people.value;
+		resultAmount.innerText = amount.toFixed(2); // push Tip Amount 
+		totalSum.innerText = summ.toFixed(2); // push Total 
 }
 
 function removeAll() {
@@ -77,6 +86,7 @@ function maxLengthCheck(object){
   if (object.value.length > object.maxLength)
 	 object.value = object.value.slice(0, object.maxLength)
 }
+
 function defaultBtnReset() {
 	if (totalSum.innerText === "0.00" || checkSum.value === ""){
 	btnReset.style.cssText = `
